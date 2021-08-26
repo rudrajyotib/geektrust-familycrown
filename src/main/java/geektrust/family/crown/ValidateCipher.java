e91895c87786c2339c36a1749f4773b73833056f
rudrajyotib <Gingerbread123!>
2021-08-26 09:38:41 +0530

    Initial commit

---
+++ src/main/java/geektrust/family/crown/ValidateCipher.java
@@ -0,0 +1,40 @@
+package geektrust.family.crown;
+
+public enum ValidateCipher {
+
+    SPACE(new int[]{1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 7),
+    LAND(new int[]{2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 5),
+    WATER(new int[]{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0}, 7),
+    ICE(new int[]{1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}, 7),
+    AIR(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, 3),
+    FIRE(new int[]{1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, 6);
+
+    private final int[] validationRule;
+    private final int decipherKey;
+
+    ValidateCipher(int[] validationRule, int decipherKey) {
+        this.validationRule = validationRule;
+        this.decipherKey = decipherKey;
+    }
+
+    public boolean isValidMessage(String message) {
+        int[] decipherMap = new int[26];
+        message = message.toUpperCase();
+        for (int i = 0; i < message.length(); i++) {
+            int charVal = message.charAt(i) - 65;
+            if ((charVal >= 0) && (charVal <= 25)) {
+                int decipheredChar = charVal - decipherKey;
+                if (decipheredChar < 0) {
+                    decipheredChar = decipheredChar + 26;
+                }
+                ++decipherMap[decipheredChar];
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (this.validationRule[i] > decipherMap[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/geektrust/family/crown/ValidateCipherTest.java
@@ -0,0 +1,28 @@
+package geektrust.family.crown;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateCipherTest {
+
+    @Test
+    public void shouldValidateCipherMessage()
+    {
+        assertTrue(ValidateCipher.AIR.isValidMessage("ROZO"));
+        assertTrue(ValidateCipher.AIR.isValidMessage("rozo"));
+        assertTrue(ValidateCipher.LAND.isValidMessage("FAIJWJSOOFAMAU"));
+        assertTrue(ValidateCipher.ICE.isValidMessage("STHSTSTVSASOS"));
+        assertFalse(ValidateCipher.AIR.isValidMessage("rizi"));
+        assertFalse(ValidateCipher.AIR.isValidMessage("OWLAOWLBOWLC"));
+        assertTrue(ValidateCipher.LAND.isValidMessage("OFBBMUFDICCSO"));
+        assertTrue(ValidateCipher.ICE.isValidMessage("VTBTBHTBBBOBAB"));
+        assertTrue(ValidateCipher.SPACE.isValidMessage("NVZPSSH"));
+        assertFalse(ValidateCipher.SPACE.isValidMessage("NVZPSTH"));
+        assertTrue(ValidateCipher.WATER.isValidMessage("XUYUYytytVJAVWBZZZZZ"));
+        assertFalse(ValidateCipher.WATER.isValidMessage("XUYUYytytVMAVWBZZZZZ"));
+        assertTrue(ValidateCipher.FIRE.isValidMessage("AJRXEGEMSSUTTTTT"));
+        assertFalse(ValidateCipher.FIRE.isValidMessage("NVZPSTH"));
+    }
+
+}
